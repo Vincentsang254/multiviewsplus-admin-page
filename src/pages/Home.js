7bca8140a42a812c783caa102ed30b130f48298f
Kimuu76 <officialkimutai@gmail.com>
2024-08-09 17:34:18 +0300

    "Deleted Header.js component and updated Home.js page with authentication check and navigation"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
 /** @format */
 
-import React from "react";
 import VideoUpload from "../components/videos/VideoUpload";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { id } = useSelector((state) => state.auth);
+
+	// Fetch tips and handle navigation based on authentication status
+	useEffect(() => {
+		if (!id) {
+			navigate("/login"); // Redirect to login if not authenticated
+		}
+	}, [id, navigate]);
 	return (
 		<div>
-			<h1>Home</h1>
 			<VideoUpload />
 		</div>
 	);
